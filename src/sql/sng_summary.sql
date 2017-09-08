73dc86829a2d08aab74769e4ddaba546e42472e5
yiwensong.com <yiwensong.com@gmail.com>
2017-09-08 06:24:34 +0000

    added PL to sng_summary view

--- src/sql/sng_summary.sql
+++ src/sql/sng_summary.sql
@@ -8,7 +8,8 @@ SELECT sng_results.id,
   sng_types.buyin,
   sng_types.rake,
   sng_types.players,
-  sng_prizes.prize
+  sng_prizes.prize,
+  sng_prizes.prize - sng_types.buyin - sng_types.rake AS pl
 FROM ((sng_results
 INNER JOIN sng_types ON sng_results.sng_id = sng_types.id)
 LEFT JOIN sng_prizes ON sng_prizes.sng_id = sng_results.sng_id
